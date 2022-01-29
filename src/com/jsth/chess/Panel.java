155d7623ee5599aa48a6a32eb5506704912805ca
Tobias Hofinger <Hofinger.Tobias@sus.htl-grieskirchen.at>
2022-01-29 17:00:29 +0100

    sdfdf

--- src/com/jsth/chess/Panel.java
+++ src/com/jsth/chess/Panel.java
@@ -11,24 +11,22 @@ import java.net.URL;
 public class Panel extends JPanel {
     int x = 55;
     int y = 45;
-    int z = 0;
+
     int size = 60;
     private static final long serialVersionUID = -12345677L;
+
     @Override
-    protected void paintChildren(Graphics graphics)
-    {
+    protected void paintChildren(Graphics graphics) {
         super.paintChildren(graphics);
-        Graphics2D graphics2D = (Graphics2D)graphics;
+        Graphics2D graphics2D = (Graphics2D) graphics;
 
         //img laden und anzeigen
         ClassLoader cl = getClass().getClassLoader();
         URL rURL = cl.getResource("/Users/jonasstarck/Desktop/POS1/AndroidStudio/Schach_Projekt/src/com/jsth/chess/img/bishop_black.png");
         System.out.println(rURL);
-        if(rURL == null)
-        {
+        if (rURL == null) {
             System.out.println("Bild wurde nicht geladen");
-        }
-        else {
+        } else {
             try {
                 File image = new File(rURL.toURI());
                 try {
@@ -46,15 +44,15 @@ public class Panel extends JPanel {
         for (int j = 0; j < 4; j++) {
             graphics2D.setColor(Color.darkGray);
             for (int i = 0; i < 4; i++) {
-                graphics2D.fillRect(x + 2 * i *size, y + (0 + 2 *j) * size, size, size);
-                graphics2D.fillRect(x + (1 + 2 *i) * size, y +(1 + 2*j) * size, size, size);
+                graphics2D.fillRect(x + 2 * i * size, y + (0 + 2 * j) * size, size, size);
+                graphics2D.fillRect(x + (1 + 2 * i) * size, y + (1 + 2 * j) * size, size, size);
             }
 
 
             graphics2D.setColor(Color.lightGray);
             for (int i = 0; i < 4; i++) {
-                graphics2D.fillRect(x + (1+2*i)*size, y + (0 + 2 *j) * size, size, size);
-                graphics2D.fillRect(x + 2 * i * size, y + (1+2*j)* size, size, size);
+                graphics2D.fillRect(x + (1 + 2 * i) * size, y + (0 + 2 * j) * size, size, size);
+                graphics2D.fillRect(x + 2 * i * size, y + (1 + 2 * j) * size, size, size);
             }
         }
     }
